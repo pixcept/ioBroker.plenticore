b90147cd09f5853b54dc765047dff2956239387e
Marius Burkard <m.burkard@pixcept.de>
2020-01-03 09:43:41 +0100

    - bumped version - added translations

--- admin/words.js
+++ admin/words.js
@@ -2,28 +2,76 @@
 'use strict';
 
 systemDictionary = {
-    'plenticore adapter settings': {
-        'en': 'Adapter settings for Plenticore',
-        'de': 'Adaptereinstellungen für Plenticore'
+    "plenticore adapter settings": {
+		"en": "Adapter settings for Plenticore",
+		"de": "Adaptereinstellungen für Plenticore",
+		"ru": "Настройки адаптера для Plenticore",
+		"pt": "Configurações do adaptador para Plenticore",
+		"nl": "Adapterinstellingen voor Plenticore",
+		"fr": "Paramètres de l'adaptateur pour Plenticore",
+		"it": "Impostazioni dell'adattatore per Plenticore",
+		"es": "Configuración del adaptador para Plenticore",
+		"pl": "Ustawienia adaptera dla Plenticore",
+		"zh-cn": "Plenticore的适配器设置"
     },
-    'ipaddress': {
-        'en': 'IP address',
-        'de': 'IP-Adresse'
-    },
-    'password': {
-        'en': 'Password for plant owner',
-        'de': 'Passwort für Anlagenbesitzer'
-    },
-    'pollinterval': {
-        'en': 'Polling interval',
-        'de': 'Polling-Intervall'
-    },
-	'on save adapter restarts with new config immediately': {
-		'en': 'on save adapter restarts with new config immediately',
-		'de': 'on save adapter restarts with new config immediately'
+    "ipaddress": {
+		"en": "IP address",
+		"de": "IP-Adresse",
+		"ru": "айпи адрес",
+		"pt": "endereço de IP",
+		"nl": "IP adres",
+		"fr": "adresse IP",
+		"it": "indirizzo IP",
+		"es": "dirección IP",
+		"pl": "adres IP",
+		"zh-cn": "IP地址"
 	},
-	'debug api requests': {
+    "password": {
+        "en": "Password for plant owner",
+		"de": "Passwort für Anlagenbesitzer",
+		"ru": "Пароль для владельца завода",
+		"pt": "Senha para o proprietário da planta",
+		"nl": "Wachtwoord voor planteneigenaar",
+		"fr": "Mot de passe pour le propriétaire de l'usine",
+		"it": "Password per il proprietario dell'impianto",
+		"es": "Contraseña para el dueño de la planta",
+		"pl": "Hasło dla właściciela zakładu",
+		"zh-cn": "工厂所有者的密码"
+    },
+    "pollinterval": {
+        "en": "Polling interval",
+		"de": "Abrufintervall",
+		"ru": "Интервал опроса",
+		"pt": "Intervalo de pesquisa",
+		"nl": "Polling-interval",
+		"fr": "Intervalle d'interrogation",
+		"it": "Intervallo di polling",
+		"es": "Intervalo de votación",
+		"pl": "Interwał odpytywania",
+		"zh-cn": "轮询间隔"
+    },
+	"on save adapter restarts with new config immediately": {
+		"en": "on save adapter restarts with new config immediately",
+		"de": "Beim Speichern wird der Adapter sofort mit der neuen Konfiguration neu gestartet",
+		"ru": "при сохранении адаптер перезапускается с новой конфигурацией немедленно",
+		"pt": "ao salvar o adaptador reinicia imediatamente com a nova configuração",
+		"nl": "on save adapter wordt onmiddellijk opnieuw gestart met de nieuwe configuratie",
+		"fr": "lors de l'enregistrement, l'adaptateur redémarre avec la nouvelle configuration immédiatement",
+		"it": "on save adapter si riavvia immediatamente con la nuova configurazione",
+		"es": "al guardar el adaptador se reinicia con la nueva configuración inmediatamente",
+		"pl": "po zapisaniu adapter natychmiast uruchamia się ponownie z nową konfiguracją",
+		"zh-cn": "保存适配器上的适配器立即以新配置重新启动"
+	},
+	"debug api requests": {
 		'en': 'Debug API requests',
-		'de': 'API-Anfragen debuggen'
+		'de': 'API-Anfragen debuggen',
+		"ru": "Отладка запросов API",
+		"pt": "Solicitações de API de depuração",
+		"nl": "Debug API-aanvragen",
+		"fr": "Déboguer les demandes d'API",
+		"it": "Richieste API di debug",
+		"es": "Depurar solicitudes de API",
+		"pl": "Debuguj żądania interfejsu API",
+		"zh-cn": "调试API请求"
 	}
 };
